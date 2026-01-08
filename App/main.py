acc5fc75b6c645b25c6d366bc1e158f7f3c40757
hemkishorepradhan2 <hemkishore.pradhan@ust.trumio.ai>
2026-01-08 16:30:33 +0530

    Added starlette status code.

--- App/main.py
+++ App/main.py
@@ -4,6 +4,7 @@ from services import list_customers as list_customers_service
 from services import remove_customer as remove_customer_service
 from services import get_customer_by_id as get_customer_service
 from schemas import CustomerCreate
+from starlette import status
 
 app = FastAPI()
 
@@ -11,7 +12,7 @@ app = FastAPI()
 async def home_route():
     return "Welcome!"
 
-@app.post("/create_customer")
+@app.post("/create_customer", status_code=status.HTTP_201_CREATED)
 async def create_customer(customer: CustomerCreate):
     create_customer_service(
         id=customer.id,
@@ -21,18 +22,18 @@ async def create_customer(customer: CustomerCreate):
     )
     return {"message": "Customer added successfully."}
 
-@app.get("/get_customers")
+@app.get("/get_customers", status_code=status.HTTP_200_OK)
 async def get_customers():
     return list_customers_service()
 
-@app.get("/customer/{customer_id}")
+@app.get("/customer/{customer_id}",status_code=status.HTTP_200_OK)
 async def get_customer_by_id(customer_id: int):
     customer = get_customer_service(customer_id)
     if not customer:
         raise HTTPException(status_code=404, detail="Customer not found")
     return {"customer": customer}
 
-@app.delete("/remove_customer/{customer_id}")
+@app.delete("/remove_customer/{customer_id}",status_code=status.HTTP_204_NO_CONTENT)
 async def remove_customer(customer_id: int):
     try:
         remove_customer_service(customer_id)
